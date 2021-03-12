94eb1a1694b64e67d41df7c702eb8ce86f3d6f67
3vilbird <devilisnotinhome@gmail.com>
2021-03-12 23:54:42 +0530

    recursion added

---
+++ 01_basic/04_clousers.js
@@ -0,0 +1,9 @@
+// what is closure
+
+// the function that has access to the variable which is out side the 
+// function
+
+
+// MDN link
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
---
+++ 01_basic/06_recursion.js
@@ -0,0 +1,48 @@
+// lets goooo
+
+
+const DrillDown = (num) => {
+    if (num === 0) return;
+    console.log(num)
+    DrillDown(num - 1);
+
+}
+
+//DrillDown(20);
+
+
+let categories = [
+    { id: "animals", 'parent': null },
+    { id: "mammals", 'parent': 'animals' },
+    { id: "cats", 'parent': 'mammals' },
+    { id: "dogs", 'parent': 'mammals' },
+    { id: "testdog1", 'parent': 'dogs' },
+    { id: "testdog2", 'parent': 'dogs' },
+    { id: "testcat1", 'parent': 'cats' },
+    { id: "testcat2", 'parent': 'cats' },
+]
+
+
+let makeFun = (categories, parent) => {
+
+    let node = {}
+    categories.filter(c => c.parent === parent)
+        .forEach(c => node[c.id] = makeFun(categories, c.id)
+        );
+
+    return node;
+}
+
+console.log(
+    JSON.stringify(makeFun(categories, null), null, 2)
+)
+
+
+
+
+
+
+
+
+
+
