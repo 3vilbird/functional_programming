bf481024485518e73bbc5103902c525bbf5303fc
3vilbird <devilisnotinhome@gmail.com>
2021-03-13 23:01:20 +0530

    added functors

--- 01_basic/08_functor.js
+++ 01_basic/08_functor.js
@@ -9,3 +9,24 @@
 
 
 // code needs to be added
+
+
+// lets get our hands durty
+
+// string function
+
+const Stringfunctor = (value, fn) => {
+    var chars = value.split("");
+    return chars.map((char) => String.fromCharCode(fn(char.charCodeAt(0)))).join("")
+}
+
+
+const plus1 = value => value + 1;
+const minus1 = value => value - 1;
+
+
+console.log([4, 5].map(plus1)) // each element is passed to 
+
+console.log(Stringfunctor("ABC", plus1));
+console.log(Stringfunctor("XYZ", minus1));
+
